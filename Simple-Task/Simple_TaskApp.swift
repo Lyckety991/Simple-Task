07593848eb3c6b6c43e768f33c41829e126d5d72
Patrick.Lanham <p.lanham91@icloud.com>
2025-01-18 08:30:01 +0100

    Initial Commit

---
+++ Simple-Task/Simple_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_TaskApp.swift
+//  Simple-Task
+//
+//  Created by Patrick Lanham on 18.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
